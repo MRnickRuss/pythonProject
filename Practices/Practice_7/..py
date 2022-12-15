c058dd351094c6cc9c5cead973875ec36ebf4fb5
MRnickRuss <nic2002@yandex.ru>
2022-12-15 16:54:51 +0300

    smt

---
+++ Practices/Practice_7/..py
@@ -0,0 +1,29 @@
+import csv
+
+
+def get_books(file, name):
+    with open(file, 'r') as f:
+        reader = csv.reader(f, delimiter='|')
+        result = [[]]
+        result.clear()
+        for row in reader:
+            if ''.join(row).__contains__(name):
+                result.append(row)
+        return result
+
+
+def get_totals(data: [[]], sum=0, add=0):
+    result = [[]]
+    result.clear()
+    for row in data:
+        count = int(row[3]) * float(row[4])
+        if count < sum:
+            sum += add
+        result.append([row[0], count])
+    return result
+
+
+books = get_books("itsnotcsv.csv", "Python")
+result = get_totals(books)
+for i in result:
+    print(i)
